d7b5c0eddc02af23600bba2150cb9ad0d8b3a5fc
Patryk Głowienko <pgw@touk.pl>
2023-03-02 10:42:22 +0100

    Max profit O(n) solutions & custom measure time function added

--- src/main/kotlin/DynamicProgramming.kt
+++ src/main/kotlin/DynamicProgramming.kt
@@ -4,9 +4,87 @@ object DynamicProgramming {
     You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
     Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
      */
-    fun maxProfit(prices: IntArray): Int {
-        //TODO implement me
-        return 0
+    fun maxProfitSimple(prices: IntArray): Int {
+        var profit = 0
+
+        for (currentBuyIndex in 0 until prices.lastIndex) {
+            for (currentSellIndex in currentBuyIndex + 1..prices.lastIndex) {
+                if (prices[currentSellIndex] - prices[currentBuyIndex] > profit)
+                    profit = prices[currentSellIndex] - prices[currentBuyIndex]
+            }
+        }
+
+        return profit
+    }
+
+    fun maxProfitFast(prices: IntArray): Int {
+        var profit = 0
+        var buyIndex = 0
+
+        for (currentIndex in 1..prices.lastIndex) {
+            val currentValue = prices[currentIndex]
+            val existingBuyValue = prices[buyIndex]
+
+            if (currentValue < existingBuyValue) {
+                buyIndex = currentIndex
+                continue
+            }
+
+            if (currentValue - existingBuyValue > profit) {
+                profit = currentValue - existingBuyValue
+            }
+        }
+
+        return profit
+    }
+
+    /*
+    On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time.
+    However, you can buy it then immediately sell it on the same day (multiple transactions possible).
+
+    So we can sell and buy and move forward for another sell on same day/index.
+
+    Solution notes: it is a must to take any profit - as we do not know if we have more or any in the nexy days/indices
+     12345
+     54339
+     */
+    fun maxProfitMultipleTransactions(prices: IntArray): Int {
+        var profit = 0
+
+        for (currentIndex in 1..prices.lastIndex) {
+            val currentValue = prices[currentIndex]
+            val buyPrice = prices[currentIndex - 1]
+            if (currentValue - buyPrice > 0) {
+                profit += currentValue - buyPrice
+            }
+        }
+
+        return profit
+    }
+
+    fun maxProfitTwoTransactions(prices: IntArray): Int { // not fully working, needs some more complex memory
+        val profits = mutableListOf(0)
+        var lowestPriceProfit = 0
+        var currentLowestPrice = prices[0]
+
+        for (currentIndex in 1..prices.lastIndex) {
+            val currentValue = prices[currentIndex]
+            val buyPrice = prices[currentIndex - 1]
+            val currentProfit = currentValue - buyPrice
+
+            if (buyPrice < currentLowestPrice) {
+                currentLowestPrice = buyPrice
+            }
+
+            val currentMaxProfit = currentValue - currentLowestPrice
+            if (currentMaxProfit > 0 && currentMaxProfit > currentProfit) {
+                lowestPriceProfit = currentMaxProfit
+            } else if (currentProfit > 0) {
+                profits.add(currentProfit)
+            }
+        }
+        profits += lowestPriceProfit
+        return profits.sortedDescending().take(2).sum()
     }
 
     /*
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,37 @@
 import ArraysAndBruteForce.allUniqueCombinationsSummingUpToTargetFinal
 import ArraysAndBruteForce.allUniqueCombinationsSummingUpToTargetFinalRefactored
+import DynamicProgramming.maxProfitFast
+import DynamicProgramming.maxProfitMultipleTransactions
+import DynamicProgramming.maxProfitSimple
+import DynamicProgramming.maxProfitTwoTransactions
 import Fibonacci.calculateFibonacci
 import Fibonacci.calculateFibonacciBottomUp
 import StringChallenges.longestCommonPrefix
 import StringChallenges.longestCommonPrefixWithReduce
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
+import kotlin.random.Random
 import kotlin.system.measureTimeMillis
 
 fun main(args: Array<String>) {
     println("SOLUTIONS:")
     println()
+    println("max profit | dynamic programming")
 
+    println(maxProfitTwoTransactions(intArrayOf(1, 2, 3, 4, 5)))
+    println(maxProfitMultipleTransactions(intArrayOf(7, 1, 5, 3, 6, 4)))
+    println(maxProfitFast(intArrayOf(7)))
+    println(maxProfitSimple(intArrayOf(7, 1, 5, 3, 6, 4)))
+
+    println("time measure maxProfitSimple O(n)) vs  maxProfitFast")
+    val bigRandomArray = IntArray(90100) { Random.nextInt(1, 1000002) }
+    println("brute force: ${executeMeasuringTimeMillis { maxProfitSimple(bigRandomArray) }} ms")
+    println("fast: ${executeMeasuringTimeMillis { maxProfitFast(bigRandomArray) }} ms")
+    println("multiple transactions: ${executeMeasuringTimeMillis { maxProfitMultipleTransactions(bigRandomArray) }} ms")
+
+    println()
+    println()
     println("fibonacci:")
     println(calculateFibonacci(10))
     println(calculateFibonacciBottomUp(10))
@@ -34,4 +56,18 @@ fun main(args: Array<String>) {
             }
         } ms "
     )
+}
+
+/**
+ * Executes given function block returning result and measured execution time in milliseconds.
+ */
+@OptIn(ExperimentalContracts::class)
+inline fun <reified T : Any> executeMeasuringTimeMillis(block: () -> T): Pair<T, Long> {
+    contract {
+        callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+    }
+    val start = System.currentTimeMillis()
+    val result = block()
+    val end = System.currentTimeMillis()
+    return Pair(result, end - start)
 }
